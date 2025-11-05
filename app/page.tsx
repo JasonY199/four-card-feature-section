4352bf74b0073b511e2a5b69689d6aa6246a44fe
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-11-05 15:02:18 -0500

    structure heading area

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,16 @@ export default function Home() {
   return (
     <>
       <main>
-        <h1>hello world</h1>
+        <div>
+          <h1>
+            Reliable, efficient delivery <span>Powered by Technology</span>
+          </h1>
+          <p>
+            Our Artificial Intelligence powered tools use millions of project
+            data points to ensure that your project is successful
+          </p>
+        </div>
+        <div></div>
       </main>
     </>
   );
