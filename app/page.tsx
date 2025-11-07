cf336012ea6ae9b9fbce1e86c12fc0dd4b464464
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-11-07 15:34:39 -0500

    style header area

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
 import FeatureCard from "@/components/FeatureCard";
 
 export default function Home() {
   return (
     <>
-      <main>
-        <div>
-          <h1>
-            Reliable, efficient delivery <span>Powered by Technology</span>
+      <main className="flex flex-col gap-15 py-14 px-7">
+        <div className="flex flex-col gap-4 items-center">
+          <h1 className="flex flex-col text-center">
+            <span className="text-preset-2">Reliable, efficient delivery</span>{" "}
+            <span className="text-preset-1">Powered by Technology</span>
           </h1>
-          <p>
+          <p className="text-preset-4 text-center">
             Our Artificial Intelligence powered tools use millions of project
             data points to ensure that your project is successful
           </p>
